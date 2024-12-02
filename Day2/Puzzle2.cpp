aeb7b5e04e42981474b13affae2a24fb6d742ee9
Skzter <v.goetze03@gmail.com>
2024-12-02 10:57:00 +0100

    test

--- Day2/Puzzle2.cpp
+++ Day2/Puzzle2.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 #include<fstream>
+#include<sstream>
 #include<string>
+#include<vector>
+#include<array>
 
+std::vector<std::vector<int>>input;
 
 void DateiEinlesen(std::string Dateiname);
+void Test(std::string Report);
 
 int main()
 {
-    DateiEinlesen("Input.txt");
+    DateiEinlesen("wtf.txt");
 }
 
 void DateiEinlesen(std::string Dateiname)
@@ -16,9 +21,22 @@ void DateiEinlesen(std::string Dateiname)
     datei.open(Dateiname, std::ios::in);
 
     std::string line;
-    int 
     while(datei.good())
     {
         std::getline(datei, line);
+        Test(line);
     }
+}
+
+void Test(std::string Report)
+{
+    std::vector<int>Zeile;
+    std::stringstream test(Report);
+    test >> Zeile;
+    for(size_t i = 0; i < Zeile.size(); i++)
+    {
+        std::cout << Zeile[i] << std::endl;
+    }
+
+    input.push_back(Zeile);
 }
